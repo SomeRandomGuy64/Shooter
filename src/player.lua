a73a8db06102f076c9b8a994470473dec7aed88f
SomeRandomGuy64 <faizan163123@gmail.com>
2022-12-29 11:34:04 +0000

    timer plus some shooting stuff to fix

--- src/player.lua
+++ src/player.lua
@@ -5,10 +5,31 @@ function Player:new(x, y)
     self.speed = 325
     self.radius = 5
     self.score = 0
+    self.shoot = 0
+    self.bPS = 30
+    self.rBPS = 1/self.bPS
+    self.time = 100
+    self.timeSpeed = 1
 end
 
 function Player:update(dt)
     Player.super.update(self, dt)
+    --timer ------------------
+    if self.time > 0 then
+        self.time = self.time - self.timeSpeed * dt
+    end
+
+    --shooting bullets -----------
+    if love.keyboard.isDown("space") then
+        self.shoot = self.shoot + 1 * dt
+        if math.floor(self.shoot) > 1 then
+            self.shoot = 0
+        end
+        if self.shoot % 1 == 0 then
+            table.insert(listOfBullets, Bullet(self.x, self.y))
+        end
+    end
+
 --controls -- also detects when the player is on the edge of the screen
     if love.keyboard.isDown("left") then
         self.x = self.x - self.speed * dt
@@ -41,7 +62,8 @@ function Player:draw()
     --sets colour to white
     love.graphics.setColor(1,1,1)
     love.graphics.circle("fill", self.x, self.y, self.radius)
-    love.graphics.print("Score " .. self.score, 0, 0)
+    love.graphics.print("Score: " .. self.score, 0, 0)
+    love.graphics.print("Time: " .. self.time, 0, 30)
 end
 
 --Creates bullets when the spacebar is pressed ------------------

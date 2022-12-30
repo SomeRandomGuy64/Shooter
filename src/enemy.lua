ca4cee75cf3f66a6a1191228d32d72bae4081aca
SomeRandomGuy64 <faizan163123@gmail.com>
2022-12-30 15:29:18 +0000

    cubic movement for enemy

--- src/enemy.lua
+++ src/enemy.lua
@@ -4,7 +4,7 @@ function Enemy:new(x, y)
     Enemy.super.new(self, x, y)
 
     self.x = 20
-    self.y = 20
+    self.y = 0.00001 * (self.x)^3 - (2.5 * self.x) + 520
 
     self.speed = 100
 
@@ -14,6 +14,7 @@ end
 function Enemy:update(dt)
     Enemy.super.update(self, dt)
     self.x = self.x + self.speed * dt
+    self.y = 0.00001 * (self.x)^3 - (2.5 * self.x) + 520
     if self.x >= 480 - self.radius  or self.x <= 0 + self.radius then
         self.speed = -self.speed
     end

36b37faf523e32dc1f412b82db8a09efe526bf65
SomeRandomGuy64 <faizan163123@gmail.com>
2023-01-05 10:19:42 +0000

    bezier curves

--- src/enemy.lua
+++ src/enemy.lua
@@ -16,6 +16,7 @@ function Enemy:new(x, y)
     self.angleX = 0
     self.angleY = 0
     self.hypotenuse = 0
+    self.oldHypotenuse = 0
 
     self.angle = 0
 end
@@ -24,16 +25,15 @@ function Enemy:update(dt)
     Enemy.super.update(self, dt)
     self.oldX = self.x
     self.oldY = self.y
+
+    self.x = self.x + self.speed * dt
     
     self.y = 0.00001 * (self.x)^3 - (2.5 * self.x) + 520
 
     self.angleX = self.x - self.oldX
     self.angleY = self.y - self.oldY
-    self.hypotenuse = math.sqrt(self.angleX^2 + self.angleY^2)
-
     
-
-
+    self.hypotenuse = math.sqrt(self.angleX^2 + self.angleY^2)
 
     if self.x >= 480 - self.radius or self.x <= 0 + self.radius then
         self.speed = -self.speed
@@ -44,4 +44,14 @@ function Enemy:draw()
     Enemy.super.draw(self)
     love.graphics.setColor(1,0,0)
     love.graphics.circle("line", self.x, self.y, self.radius)
+
+    local controlPoints = {20,20,240,1260,460,20}
+    local curve = love.math.newBezierCurve(controlPoints)
+    local time = love.timer.getTime()
+    local loopTime = 4
+    local t = (time / loopTime) % 1
+    local x,y = curve:evaluate(t)
+
+    love.graphics.circle("fill", x, y, self.radius)
+    love.graphics.line(curve:render())
 end
